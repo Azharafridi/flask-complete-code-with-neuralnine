f48beaa47d11b87ef1926d64e3709254c49c54db
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2024-12-21 11:06:07 +0500

    updated the code in html_templete_app.py with adding different templates and make a new file filters.html

--- html_templete_app.py
+++ html_templete_app.py
@@ -11,6 +11,23 @@ def index():
 def other():
     return render_template('other.html')
 
+@app.route('/filters')
+def filters():
+    some_text = "Hello World"
+    return render_template('filters.html', some_text = some_text)
+
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1] 
+
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s * times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=5555, debug=True)
